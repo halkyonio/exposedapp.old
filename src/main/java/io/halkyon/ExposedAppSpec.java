e1cbfe0bac33ad08620b10177391fca362508dbc
Chris Laprun <metacosm@gmail.com>
2021-08-03 13:24:48 +0200

    feat: add imageRef field

--- src/main/java/io/halkyon/ExposedAppSpec.java
+++ src/main/java/io/halkyon/ExposedAppSpec.java
@@ -3,4 +3,13 @@ package io.halkyon;
 public class ExposedAppSpec {
 
     // Add Spec information here
+    private String imageRef;
+
+    public String getImageRef() {
+        return imageRef;
+    }
+
+    public void setImageRef(String imageRef) {
+        this.imageRef = imageRef;
+    }
 }
